f41be13629397c7932597a9e9bc181aaa8c5dd7c
MD. Nurul Huda <nurulhuda5801@gmail.com>
2024-07-09 17:18:47 +0600

    add some echo

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 
+rm -rf img 
+echo "Downloading  Neovim....."
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+echo "Installing Neovim...."
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 export PATH="$PATH:/opt/nvim-linux64/bin"
